f93181c49dde8af9aa1978a1edaf99c723076233
carlos Hernandez <carloskant@hotmail.com>
2025-07-21 19:26:29 -0500

    cargar proyecto función cuadratica

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculadora en Python
+Este proyecto contiene una calculadora
+básica en Python que permite determinar las raices
---
+++ raizcuadratica.py
@@ -0,0 +1,26 @@
+import math
+
+def resolver_ecuacion_cuadratica(a, b, c):
+   
+    discriminante = (b**2) - 4*(a*c)
+
+    if discriminante > 0:
+ 
+        x1 = (-b - math.sqrt(discriminante))/(2*a)
+        x2 = (-b + math.sqrt(discriminante))/(2*a)
+        return(x1,x2)
+    elif discriminante ==0:
+        x = -b / (2*a)
+        return(x,x)
+    else:
+        parte_real= -b / (2*a)
+        parte_imaginaria=math.sqrt(-discriminante)/(2*a)
+        x1 = complex(parte_real, parte_imaginaria)
+        return(x1, x2)
+
+a= 1
+b= -5
+c= 6
+
+soluciones = resolver_ecuacion_cuadratica(a, b, c)
+print ("las solucines son:{soluciones}")
